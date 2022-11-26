37ba0a487e602a53a605eb53b8d66b5aac17a935
Ayoub Ait Belhaj <aaitbelh@e2r12p9.1337.ma>
2022-11-26 22:11:33 +0100

    cassssper

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.cpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaitbelh <aaitbelh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/20 18:00:35 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/11/23 22:14:11 by aaitbelh         ###   ########.fr       */
+/*   Created: 2022/11/20 18:00:29 by aaitbelh          #+#    #+#             */
+/*   Updated: 2022/11/26 19:52:46 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <stack>
+#include <typeinfo>
+#include <iterator>
 #include "vector.hpp"
+int main()
+{
+  ft::vector<int> a;
+  std::vector<int> lool;
+  std::allocator<int>
+}
--- vector.hpp
+++ vector.hpp
@@ -1,25 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.hpp                                         :+:      :+:    :+:   */
+/*   vector.hpp                                         :+:      :+:    :+:   */    
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaitbelh <aaitbelh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aaitbelh <aaitbelh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/08 17:35:55 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/09/09 11:29:26 by aaitbelh         ###   ########.fr       */
+/*   Created: 2022/11/20 18:00:33 by aaitbelh          #+#    #+#             */
+/*   Updated: 2022/11/23 22:25:25 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_HPP
-#define VECTOR_HPP
-
-#include <iostream>
-#include <string>
-
-
-
-template<typename T> class vector
+#ifndef FT_VECTOR_HPP
+#define FT_VECTOR_HPP
+#include <memory>
+namespace ft
 {
+    //---------------------------------------iterator-----
+    template <class _TYPE>
+    class __vector_iterators
+    {
+        public:
+            typedef _TYPE 		                                                    iterator_type;
+            typedef typename std::iterator_traits<iterator_type>::iterator_category iterator_category;
+            typedef typename std::iterator_traits<iterator_type>::value_type        value_type;
+            typedef typename std::iterator_traits<iterator_type>::difference_type   difference_type;
+            typedef typename std::iterator_traits<iterator_type>::pointer           pointer;
+            typedef typename std::iterator_traits<iterator_type>::reference         reference;
+			pointer																	it_ptr;
+            __vector_iterators operator++(int){ __vector_iterators tmp(*this); this->it_ptr++; return (tmp); }
+            __vector_iterators& operator++() { this->it_ptr++; return (*this); }
+            __vector_iterators operator--(int){ __vector_iterators tmp(*this); this->it_ptr--; return (tmp); }
+            __vector_iterators& operator--() { this->it_ptr--; return (*this); }
+            __vector_iterators& operator=(__vector_iterators& obj) { this->it_ptr = obj.it_ptr; return (*this); }
+            __vector_iterators& operator+=(ptrdiff_t n) { this->it_ptr += n; return (*this); }
+            __vector_iterators& operator-=(ptrdiff_t n) { this->it_ptr -= n; return (*this); }
+            bool operator==(__vector_iterators& obj) { return (this->it_ptr == obj.it_ptr ? 1 : 0); }
+            bool operator!=(__vector_iterators& obj) { return (this->it_ptr != obj.it_ptr ? 1 : 0); }
+            bool operator>(__vector_iterators& obj) { return (this->it_ptr > obj.it_ptr ? 1 : 0); }
+            bool operator<(__vector_iterators& obj) { return (this->it_ptr < obj.it_ptr ? 1 : 0); }
+            bool operator>=(__vector_iterators& obj) { return (this->it_ptr >= obj.it_ptr ? 1 : 0); }
+            bool operator<=(__vector_iterators& obj) { return (this->it_ptr <= obj.it_ptr ? 1 : 0); }
+
+    };
+    template <class T, class Allocator = std::allocator<T> >
+    class vector
+    {
+        public:
+            typedef T                                                     value_type;
+            typedef Allocator                                             allocator_type;
+            typedef typename allocator_type::pointer                      pointer;
+            typedef class __vector_iterators<pointer>                     iterator;
+			iterator													  v_ptr;
+            vector(){ this->v_ptr.it_ptr = NULL; }
+            iterator& begin() { return (this->v_ptr); }
+
+    };
     
 };
 
